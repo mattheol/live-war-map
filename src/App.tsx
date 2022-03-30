83a792f20c498f6904a10a72547e8c604da9eccb
Mateusz Olsztyński <matiol1@hotmail.com>
2022-03-30 21:04:08 +0200

    chore: test api

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import MapElement from "./components/MapElement/MapElement";
+import { config as TwitterConfig } from "./config/twitterConfig";
 
 function App() {
   return (
@@ -9,7 +9,34 @@ function App() {
       <div className="map-container">
         <MapElement />
       </div>
-      <div className="right-panel"></div>
+      <div className="right-panel">
+        <button
+          onClick={async () => {
+            const params = new URLSearchParams({
+              q: "'war'",
+              count: "100",
+              result_type: "recent",
+              geocode: "49,32,500km",
+            });
+            const res = await fetch(
+              `http://localhost:8000/https://api.twitter.com/1.1/search/tweets.json?${params.toString()}`,
+              {
+                headers: {
+                  Authorization: `Bearer ${TwitterConfig.token}`,
+                },
+              }
+            ).then((r) => r.json());
+            const filtered = res.statuses.filter((st: any) => st.geo);
+            console.log(
+              res.statuses.map((st: any) =>
+                st.geo ? st.geo : st.user.location
+              )
+            );
+          }}
+        >
+          Fetch tweets
+        </button>
+      </div>
     </div>
   );
 }
---
+++ src/components/NewsList/NewsList.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "NewsList.css";
+
+const NewsList = () => {
+  return <div></div>;
+};
+
+export default NewsList;
